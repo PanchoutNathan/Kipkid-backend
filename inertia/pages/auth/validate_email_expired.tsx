c55dcd3ee5280eba8b19544540c8fcda843ab0e6
NathanPanchout <nathanpanchout@hotmail.com>
2024-08-21 12:56:50 +0200

    wip deploy docker

--- inertia/pages/auth/validate_email_expired.tsx
+++ inertia/pages/auth/validate_email_expired.tsx
@@ -1,12 +1,15 @@
 import { Head } from '@inertiajs/react'
+import { useState } from 'react'
 
 export default function ValidateEmailExpired() {
+  const [count, setCount] = useState(0)
   return (
     <>
       <Head title="Homepage" />
 
       <div className="flex flex-col justify-center items-center h-full">
-        <div className="">L'email est expiré en vrai wesh</div>
+        <div className="">L'email est expiré en vrai wesh {count}</div>
+        <button onClick={() => setCount(count + 1)}>Count</button>
       </div>
     </>
   )
